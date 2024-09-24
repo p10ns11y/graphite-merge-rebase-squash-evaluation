d0dd656a5370d8c00c98b1c6dbaf31382cf8dfcb
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2024-09-24 10:44:14 +0200

    Merge strategy stack second pr

---
+++ samplecode/lib/sameplecode-as-executable.exs
@@ -0,0 +1,10 @@
+defmodule SamplecodeScript do
+  def main() do
+    IO.puts("How are you?")
+  end
+end
+
+SamplecodeScript.main()
+
+# In terminal, run the following command to execute the script:
+# elixir samplecode-as-executable.exs
